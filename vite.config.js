6df4e65bd15831693f3ad387313709c30920333a
henrique <henriquebusiness14@outlook.com>
2026-07-01 19:30:41 -0300

    Codigo 100 por cento corrigido

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração corrigida com a barra inicial e final obrigatórias
 export default defineConfig({
   plugins: [react()],
   base: '/gerenciador-de-estudos/',
+  build: {
+    outDir: 'dist'
+  }
 })
