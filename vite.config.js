663617f5a9e43afb4db846f7ba206f289ee969f1
henrique <henriquebusiness14@outlook.com>
2026-07-01 19:23:40 -0300

    Ajustando a base de caminhos do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração para o Vite entender o caminho do GitHub Pages
+// Configuração corrigida com a barra inicial e final obrigatórias
 export default defineConfig({
   plugins: [react()],
   base: '/gerenciador-de-estudos/',
